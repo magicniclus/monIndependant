e8dc4cfc0f44a691bdc7bf169448c22b8c0008cb
Nicolas <casteranicolas.contact@gmail.com>
2022-09-14 14:40:44 +0200

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "./_footer.scss";
+import logo from "../../assets/logowhite.png"
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            
-        </div>
+        <footer>
+            <div className="leftContainer">
+                <div className="textContainer">
+                    <p>Conditions d'utilisation</p>
+                    <p>Nous contacter</p>
+                </div>
+            </div>
+            <div className="rightContainer">
+                <img src={logo} alt="logo nicolas castera" />
+            </div>
+        </footer>
     );
 };
 
