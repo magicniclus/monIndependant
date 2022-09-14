65297466f01ff322dba7e9e1075dd44b9033adc6
Nicolas <casteranicolas.contact@gmail.com>
2022-09-14 12:01:31 +0200

    Update NavBar.js

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./_navBar.scss";
-import moi from "../../assets/nc.png"
+import logo from "../../assets/nc.png"
 
 const NavBar = () => {
     return (
         <header>
             <div className="navBar">
-                <img src={moi} alt="Nicolas CASTERA" />
+                <img src={logo} alt="logo nicolas castera" />
                 <h2>Vous vendez votre bien? <span>Laissez moi vous conseiller</span></h2>
             </div>
         </header>
