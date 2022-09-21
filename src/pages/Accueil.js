a0ee0ca9d6b6c527dcf07c112f34a1b154d746a1
Nicolas <casteranicolas.contact@gmail.com>
2022-09-21 19:33:57 +0200

    Update Accueil.js

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import NavBar from "../components/navBar/NavBar";
-import Footer from "../components/footer/Footer";
 import TopMainContainer from '../components/main/TopMainContainer';
 
 const Accueil = () => {
     return (
         <>  
-            <NavBar />
-            <main>
                 <TopMainContainer />
-            </main>
-            <Footer />
         </>
     );
 };
