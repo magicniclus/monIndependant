4275e11ed05d59bf4c18b812db7026fd55056ddf
Nicolas <casteranicolas.contact@gmail.com>
2022-09-21 19:34:13 +0200

    Create Faq.js

---
+++ src/pages/FAQ/Faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./_faq.scss";
+
+const Faq = () => {
+    return (
+        <div className='faq'>
+            <h1>FAQ</h1>
+        </div>
+    );
+};
+
+export default Faq;
