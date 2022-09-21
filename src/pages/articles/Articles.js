228a9b368d8ad58f6cd4d06b04b47550408908fa
Nicolas <casteranicolas.contact@gmail.com>
2022-09-21 19:34:01 +0200

    Create Articles.js

---
+++ src/pages/articles/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./_articles.scss";
+
+const Articles = () => {
+    return (
+        <div className='articles'>
+            <h1>Articles</h1>
+        </div>
+    );
+};
+
+export default Articles;
