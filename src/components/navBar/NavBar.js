b75e05cef50ed7a95f14fd6ca0804c39d415216a
Nicolas <casteranicolas.contact@gmail.com>
2022-09-21 19:33:55 +0200

    Update NavBar.js

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./_navBar.scss";
-import logo from "../../assets/nc.png"
+import logo from "../../assets/nc.png";
+import { NavLink} from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <header>
             <div className="navBar">
-                <img src={logo} alt="logo nicolas castera" />
+                <NavLink to="/accueil"><img src={logo} alt="logo nicolas castera" /></NavLink>
                 <h2>Vous vendez votre bien? <span>Laissez moi vous conseiller</span></h2>
             </div>
         </header>
