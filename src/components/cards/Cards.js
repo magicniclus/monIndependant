5ee16118e2dfce9943febf540d77d3dc10c81620
Nicolas <casteranicolas.contact@gmail.com>
2022-09-22 21:32:11 +0200

    Create Cards.js

---
+++ src/components/cards/Cards.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./_cards.scss";
+
+const Cards = (props) => {
+    const data = props.data
+
+    return (
+        <div className='cards'>
+            <h4 className='titleCard'>{data.qaurtier}</h4>
+            <div className="priceContainer">
+                <div className="houseContainer logement">
+                    <p className='house logementTitle'>Maison:</p>
+                    <p className='logementPrice'>{data.maison}€/m²</p>
+                </div>
+                <div className="appartmentContainer logement">
+                    <p className='appartment logementTitle'>Appartement:</p>
+                    <p className='logementPrice'>{data.appartement}€/m²</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
