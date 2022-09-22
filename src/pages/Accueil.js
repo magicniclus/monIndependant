19e0348a44844651480f0e82c91e14121d9da87a
Nicolas <casteranicolas.contact@gmail.com>
2022-09-22 21:32:18 +0200

    Update Accueil.js

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import TopMainContainer from '../components/main/TopMainContainer';
-import { city } from '../utils/city';
 
 const Accueil = () => {
-    console.log(city);
     return (
         <>  
-                <TopMainContainer />
+            <TopMainContainer />
         </>
     );
 };
