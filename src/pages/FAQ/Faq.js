37870d808dde8633a313fc7caad8a3ef0c1b8462
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 12:17:51 +0200

    Update Faq.js

--- src/pages/FAQ/Faq.js
+++ src/pages/FAQ/Faq.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { questions } from '../../utils/questions';
 import "./_faq.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Faq = () => {
+
+    const sendMail = ()=>{
+        window.open('mailto:casteranicolas.contact@gmail.com?subject=Estimation')
+    }
+    
     return (
         <div className='faq'>
-            <h1>FAQ</h1>
+            <div className="imgContainer">
+            </div>
+            <div className="content">
+                <h1>FAQ</h1>
+                {
+                    questions.map((el, idx)=>
+                        <div className="questionContainer" key={idx}>
+                            <h2>{el.question}</h2>
+                            <p>{el.reponse}</p>
+                        </div>
+                    )
+                }
+            </div>
+            <div className="contact">
+                <div className="bottomContainer">
+                    <h3>Pour une estimation <span>100% gratuite et personnalisée</span>:</h3>
+                    <div className="buttonContainer"> 
+                        <a href="tel:0631420045" onSubmit={(e)=>e.preventDefault()} className='phone'> 
+                            <FontAwesomeIcon icon={faPhone} color="white" />  
+                            0631420045
+                        </a >
+                        <button onClick={sendMail} onSubmit={(e)=>e.preventDefault()} className='mail' type='mail'> 
+                            <FontAwesomeIcon icon={faEnvelope} color="#434343" />  
+                            Mail
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
