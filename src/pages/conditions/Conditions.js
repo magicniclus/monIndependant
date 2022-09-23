253ef29291087142b6c7dc3a45c75a9d1558d14e
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 15:07:00 +0200

    Update Conditions.js

--- src/pages/conditions/Conditions.js
+++ src/pages/conditions/Conditions.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./_conditions.scss"
 
 const Conditions = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0); 
+    }, [])
     return (
         <div className='conditions'>
             <div className="imgContainer">
