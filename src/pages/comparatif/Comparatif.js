01077b89ca29672042e068b083fee7273a55d81e
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 13:50:53 +0200

    Update Comparatif.js

--- src/pages/comparatif/Comparatif.js
+++ src/pages/comparatif/Comparatif.js
@@ -1,19 +1,80 @@
 import React, { useEffect } from 'react';
 import "./_comparatif.scss";
 import {city} from "../../utils/city"
+import Cards from '../../components/cards/Cards';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Comparatif = () => {
 
+    const sendMail = ()=>{
+        window.open('mailto:casteranicolas.contact@gmail.com?subject=Estimation')
+    }
+
     return (
         <div className='comparatif'>
+            <div className="imgContainer">
+            </div>
             <div className="title">
                 <h1>Comparatif</h1>
                 <p>Ce comparatif est réaliser grace à différentes données (notarials, differents site d'annonce immobilière). Il a été réalisé sur le secteur de bordeaux, son agglomeration et sur le bassin d'Aracachon. Il est mis à jour le plus régulièrement possible.</p>
             </div>
             <div className="cardContainer">
-                {
-                    city.gironde.procheBordeaux.map(el=>console.log(el))
-                }
+                <div className="Bordeaux city">
+                    <div className="titleCity">
+                        <h2>Bordeaux</h2>
+                        {/* <p>Desciption</p> */}
+                    </div>
+                    {
+                        city.gironde.bordeaux.map(el=>{
+                            return(
+                                <Cards data={el} />
+                            )
+                        })
+                    }
+                </div>
+                <div className="agglomeration city">
+                    <div className="titleCity">
+                        <h2>Proche Bordeaux</h2>
+                        {/* <p>Desciption</p> */}
+                    </div>
+                    {
+                        city.gironde.procheBordeaux.map(el=>{
+                            return(
+                                <Cards data={el} />
+                            )
+                        })
+                    }
+                </div>
+                <div className="bassin city">
+                    <div className="titleCity">
+                        <h2>Bassin d'Arcachon</h2>
+                        {/* <p>Desciption</p> */}
+                    </div>
+                    {
+                        city.gironde.bassin.map(el=>{
+                            return(
+                                <Cards data={el} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className="contact">
+                <div className="bottomContainer">
+                    <h3>Pour une estimation <span>100% gratuite et personnalisée</span>:</h3>
+                    <div className="buttonContainer"> 
+                        <a href="tel:0631420045" onSubmit={(e)=>e.preventDefault()} className='phone'> 
+                            <FontAwesomeIcon icon={faPhone} color="white" />  
+                            0631420045
+                        </a >
+                        <button onClick={sendMail} onSubmit={(e)=>e.preventDefault()} className='mail' type='mail'> 
+                            <FontAwesomeIcon icon={faEnvelope} color="#434343" />  
+                            Mail
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
