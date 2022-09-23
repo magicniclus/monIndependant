1b6315d2a00ebc7e696bfbd85a4ec02680477d7e
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 10:02:10 +0200

    Create questions.js

---
+++ src/utils/questions.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        question: "Qui suis-je :",
+        reponse: "Spécialiste en immobilier depuis 6 ans à Bordeaux, son agglomération."
+    },
+    {
+        question: "Mes préstations :",
+        reponse: "Je commercialise les biens de mes clients en offrant une prestation englobant toute la chaine de valeur d’un projet immobilier : Estimation, conseil en optimisation fiscale, Mise en valeur, Publicité, sélection et suivi les acheteurs, visites, accompagnement jusqu’à la signature du bien."
+    },
+    {
+        question: "Domaine de compétence :",
+        reponse: "Immobilier neuf, ancien, investissement."
+    },
+    {
+        question: "Optimisation fiscale : ",
+        reponse: "Je peux vous conseiller dans une problématique fiscale via des leviers immobiliers. Plusieurs s’offrent à vous en fonction de votre situation : LMNP, Colocation,  Pinel, Malraux etc.. Suivant vos besoins et pour vous offrir des services sur-mesure et complet, je suis entouré d’expert : comptable, avocat, professionnel du bâtiment."
+    },
+    {
+        question: "Lieux d’exercice :",
+        reponse: "Bordeaux, son agglomération, le bassin d’Arcachon, et pour certains biens ou besoin il m’arrive d’exercer à Paris."
+    },
+]
