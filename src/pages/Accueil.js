aacfc78e5191c44277b2d309e01dcb2cb68b7fbb
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 15:06:49 +0200

    Update Accueil.js

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TopMainContainer from '../components/main/TopMainContainer';
 
 const Accueil = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0); 
+    }, [])
+    
     return (
         <>  
             <TopMainContainer />
