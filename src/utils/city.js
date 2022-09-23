f159952f39ba97eec773c72623697ff074d3ca1a
Nicolas <casteranicolas.contact@gmail.com>
2022-09-23 13:03:31 +0200

    Update city.js

--- src/utils/city.js
+++ src/utils/city.js
@@ -1,185 +1,217 @@
 export const city = {
     gironde:{
-        bordeaux: {
-            bastide: {
+        bordeaux: [
+            {
+                bastide: {
                 qaurtier: "La Bastide",
                 image: null,
                 appartement: 4441,
                 maison: 6021
+            }
+            },{
+                bacalan: {
+                    qaurtier: "Bacalan",
+                    image: null,
+                    appartement: 4228,
+                    maison: 4648
+                },
+            },{
+                gare: {
+                    qaurtier: "Gare Saint-Jean",
+                    image: null,
+                    appartement: 4434,
+                    maison: 5648
+                },
+            },{
+                saintAugustin: {
+                    qaurtier: "Saint Augustin",
+                    image: null,
+                    appartement: 4384,
+                    maison: 5960
+                },
+            },{
+                chartron: {
+                    qaurtier: "Les Chartrons",
+                    image: null,
+                    appartement: 5456,
+                    maison: 5960
+                },
+            },{
+                centre: {
+                    qaurtier: "Bordeaux Centre",
+                    image: null,
+                    appartement: 4849,
+                    maison: 6021
+                }
+            }
+        ],
+        procheBordeaux: [
+            {
+                merignac: {
+                    qaurtier: "Merignac",
+                    image: null,
+                    appartement: 4332,
+                    maison: 4747
+                },     
+            }, {
+                    pessac: {
+                        qaurtier: "Pessac",
+                        image: null,
+                        appartement: 3716,
+                        maison: 4709
+                    },
+                }, {
+                    Talence: {
+                        qaurtier: "Talence",
+                        image: null,
+                        appartement: 4112,
+                        maison: 5784
+                    }
+                }, {
+                    bouscat: {
+                        qaurtier: "Le Bouscat",
+                        image: null,
+                        appartement: 4201,
+                        maison: 6529
+                    }
+                }, {
+                    bruges: {
+                        qaurtier: "Bruges",
+                        image: null,
+                        appartement: 3801,
+                        maison: 4894
+                    }
+                }, {
+                    begles: {
+                        qaurtier: "Bègles",
+                        image: null,
+                        appartement: 3734,
+                        maison: 5182
+                    }
+                }, {
+                    bastide: {
+                        qaurtier: "La Bastide",
+                        image: null,
+                        appartement: 4441,
+                        maison: 6021
+                    }
+                }, {
+                    villenave: {
+                        qaurtier: "Villenave d'Ornon",
+                        image: null,
+                        appartement: 3470,
+                        maison: 4176
+                    }
+                }, {
+                    eysine: {
+                        qaurtier: "Eysine",
+                        image: null,
+                        appartement: 3676,
+                        maison: 4322
+                    }
+                }, {
+                    haillan: {
+                        qaurtier: "Le Haillan",
+                        image: null,
+                        appartement: 3941,
+                        maison: 4372
+                    }
+                }, {
+                    taillan: {
+                        qaurtier: "Le Taillan Médoc",
+                        image: null,
+                        appartement: 3890,
+                        maison: 4015
+                    }
+                }, {
+                    floirac: {
+                        qaurtier: "Floirac",
+                        image: null,
+                        appartement: 3440,
+                        maison: 3896
+                    }
+                }, {
+                    cenon: {
+                        qaurtier: "Cenon",
+                        image: null,
+                        appartement: 3137,
+                        maison: 4197
+                    }
+                }, {
+                    lormont: {
+                        qaurtier: "Lormont",
+                        image: null,
+                        appartement: 2630,
+                        maison: 3734
+                    }
+                }, {
+                    bouliac: {
+                        qaurtier: "Bouliac",
+                        image: null,
+                        appartement: 3048,
+                        maison: 4774
+                    }
+                }, {
+                    artigues: {
+                        qaurtier: "Artigues-prêt^Bordeaux",
+                        image: null,
+                        appartement: 3463,
+                        maison: 3712
+                    }
+                }, {
+                    bassens: {
+                        qaurtier: "Bassens",
+                        image: null,
+                        appartement: 3657,
+                        maison: 3432
+                    }
+                }, {
+                    carbon: {
+                        qaurtier: "Carbon-Blanc",
+                        image: null,
+                        appartement: 4242,
+                        maison: 3411
+                    }
+                }, {
+                    cubzac: {
+                        qaurtier: "Saint-André-de-Cubzac",
+                        image: null,
+                        appartement: 2389,
+                        maison: 2833
+                    },
+                }, {
+                    libourne: {
+                        qaurtier: "Libourne",
+                        image: null,
+                        appartement: 2311,
+                        maison: 2979
+                    }
+                }, {
+                    saintEmilion: {
+                        qaurtier: "Saint-Émilion",
+                        image: null,
+                        appartement: null,
+                        maison: 3465
+                    }
+                },
+            ],   
+        bassin:[
+            {
+                arcachon: {
+                    qaurtier: "Arcachon",
+                    image: null,
+                    appartement: 7567,
+                    maison: 9064
+                },
             },
-            bacalan: {
-                qaurtier: "Bacalan",
-                image: null,
-                appartement: 4228,
-                maison: 4648
+            {
+                ferret: {
+                    qaurtier: "Cap-Ferret",
+                    image: null,
+                    appartement: 9161,
+                    maison: 11746
+                },
             },
-            gare: {
-                qaurtier: "Gare Saint-Jean",
-                image: null,
-                appartement: 4434,
-                maison: 5648
-            },
-            saintAugustin: {
-                qaurtier: "Saint Augustin",
-                image: null,
-                appartement: 4384,
-                maison: 5960
-            },
-            chartron: {
-                qaurtier: "Les Chartrons",
-                image: null,
-                appartement: 5456,
-                maison: 5960
-            },
-            centre: {
-                qaurtier: "Bordeaux Centre",
-                image: null,
-                appartement: 4849,
-                maison: 6021
-            },
-        },
-        procheBordeaux: {
-            merignac: {
-                qaurtier: "Merignac",
-                image: null,
-                appartement: 4332,
-                maison: 4747
-            },
-            pessac: {
-                qaurtier: "Pessac",
-                image: null,
-                appartement: 3716,
-                maison: 4709
-            },
-            Talence: {
-                qaurtier: "Talence",
-                image: null,
-                appartement: 4112,
-                maison: 5784
-            },
-            bouscat: {
-                qaurtier: "Le Bouscat",
-                image: null,
-                appartement: 4201,
-                maison: 6529
-            },
-            bruges: {
-                qaurtier: "Bruges",
-                image: null,
-                appartement: 3801,
-                maison: 4894
-            },
-            begles: {
-                qaurtier: "Bègles",
-                image: null,
-                appartement: 3734,
-                maison: 5182
-            },
-            bastide: {
-                qaurtier: "La Bastide",
-                image: null,
-                appartement: 4441,
-                maison: 6021
-            },
-            villenave: {
-                qaurtier: "Villenave d'Ornon",
-                image: null,
-                appartement: 3470,
-                maison: 4176
-            },
-            eysine: {
-                qaurtier: "Eysine",
-                image: null,
-                appartement: 3676,
-                maison: 4322
-            },
-            haillan: {
-                qaurtier: "Le Haillan",
-                image: null,
-                appartement: 3941,
-                maison: 4372
-            },
-            taillan: {
-                qaurtier: "Le Taillan Médoc",
-                image: null,
-                appartement: 3890,
-                maison: 4015
-            },
-            floirac: {
-                qaurtier: "Floirac",
-                image: null,
-                appartement: 3440,
-                maison: 3896
-            },
-            cenon: {
-                qaurtier: "Cenon",
-                image: null,
-                appartement: 3137,
-                maison: 4197
-            },
-            lormont: {
-                qaurtier: "Lormont",
-                image: null,
-                appartement: 2630,
-                maison: 3734
-            },
-            bouliac: {
-                qaurtier: "Bouliac",
-                image: null,
-                appartement: 3048,
-                maison: 4774
-            },
-            artigues: {
-                qaurtier: "Artigues-prêt^Bordeaux",
-                image: null,
-                appartement: 3463,
-                maison: 3712
-            },
-            bassens: {
-                qaurtier: "Bassens",
-                image: null,
-                appartement: 3657,
-                maison: 3432
-            },
-            carbon: {
-                qaurtier: "Carbon-Blanc",
-                image: null,
-                appartement: 4242,
-                maison: 3411
-            },
-            cubzac: {
-                qaurtier: "Saint-André-de-Cubzac",
-                image: null,
-                appartement: 2389,
-                maison: 2833
-            },
-            libourne: {
-                qaurtier: "Libourne",
-                image: null,
-                appartement: 2311,
-                maison: 2979
-            },
-            saintEmilion: {
-                qaurtier: "Saint-Émilion",
-                image: null,
-                appartement: null,
-                maison: 3465
-            },
-
-        },
-        bassin: {
-            arcachon: {
-                qaurtier: "Arcachon",
-                image: null,
-                appartement: 7567,
-                maison: 9064
-            },
-            ferret: {
-                qaurtier: "Cap-Ferret",
-                image: null,
-                appartement: 9161,
-                maison: 11746
-            },
-        }
-    }
+        ] 
+    },
 }
